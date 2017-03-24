fd2f95e9cc3d8ce4ea9dfad66995a2d753d164bf
Stefan Gojan <vollekannehoschi@gmail.com>
2017-03-24 09:12:19 +0100

    change config to object back, because this is the test case we need

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var prefixer = require('autoprefixer')({
   browsers: ['Explorer >= 11', 'Edge >= 12', 'Firefox >= 34', 'Chrome >= 40', 'Safari >= 9'],
 })
 
-module.exports = () => ({
+module.exports = {
   plugins: [
     prefixer
   ],
-});
+};
